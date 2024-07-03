7b03703552a06014d8e01bff2cbaf2242f623a6e
Manoah1401 <manoahnob@gmail.com>
2024-07-03 17:10:37 +0530

    new

---
+++ patterns/equalrc.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int number;
+    cin >> number;
+    for(int i=0;i<number;i++){
+        for(int j=0;j<number;j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
