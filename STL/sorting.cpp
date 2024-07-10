923504893af0dfe135475225e879483633f58af1
Manoah1401 <manoahnob@gmail.com>
2024-07-11 01:50:54 +0530

    sort

---
+++ STL/sorting.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5];
+    arr[0]=100;
+    arr[1]=2;
+    arr[2]=10;
+    arr[3]=73;
+    arr[4]=4;
+
+    sort(arr,arr+5);
+    for(auto i=0;i<5;i++){
+        // cout << arr[i] << endl;
+    }
+
+    
+    
+    return 0;
+}
