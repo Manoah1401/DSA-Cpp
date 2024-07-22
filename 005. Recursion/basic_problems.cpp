b6e372333534fcc5e176f2566b55ee4efa886d6d
Manoah1401 <manoahnob@gmail.com>
2024-07-22 21:58:09 +0530

    basic_problems

---
+++ 005. Recursion/basic_problems.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void name(int i, int n){
+    if(i>n){
+        return;
+    }
+    name(i+1,n);
+    cout << "Manoah" << endl;
+}
+// reverse
+void reverse(int i, int n){
+    if(i<1){
+        return;
+    }
+    cout << i << endl;
+    reverse(i-1,n);
+}
+//backtracking
+
+void back(int i, int n){
+    if(i<1){
+        return;
+    }
+    back(i-1,n);
+    cout << i << endl;
+}
+
+int main(){
+    int n=0;
+    cin >> n;
+    // name(1,n);
+    // reverse(n,n);
+    // back(n,n);
+}
